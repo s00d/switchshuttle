1c592b79fdb1f4ed4afce32b80bea0b60119c17c
Pavel Kuzmin <Virus191288@gmail.com>
2024-08-04 10:36:11 +0500

    refactor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -45,16 +45,22 @@ fn main() {
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
         .plugin(tauri_plugin_context_menu::init())
         .on_system_tray_event(handle_system_tray_event)
-        .on_window_event(|event| {
-            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
                 event.window().hide().unwrap();
                 api.prevent_close();
             }
+            _ => {}
         })
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
     hotkeys::register_global_hotkeys(app.app_handle());
 
-    app.run(|_app_handle, _event| {});
+    app.run(|_app_handle, event| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => {}
+    });
 }
