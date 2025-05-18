bdadf08a4cdad7a9a68e5114ebf6df837d496a88
Pavel Kuzmin <Virus191288@gmail.com>
2025-05-18 14:42:45 +0500

    feat(config): add Tailwind CSS support to Vite configuration
    Integrate `@tailwindcss/vite` plugin into the Vite configuration to
    enable Tailwind CSS styling capabilities. This change enhances the
    development experience by allowing the use of Tailwind's utility-first
    CSS framework in the project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
+      tailwindcss(),
       // Only include Vue Devtools plugin in development mode
       isDev && vueDevTools()
     ].filter(Boolean),
