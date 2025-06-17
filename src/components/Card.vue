03d708fba92f1151da38f5a5402b1045fdf9f2ad
Pavel Kuzmin <Virus191288@gmail.com>
2025-06-17 20:39:07 +0500

    feat(Card): redesign

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div 
+    :class="[
+      'bg-white border border-slate-300 transition-all duration-200',
+      hover && 'hover:border-slate-400 hover:shadow-md',
+      padding && padding,
+      !padding && 'p-4'
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  hover?: boolean;
+  padding?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  hover: true,
+  padding: ''
+});
+</script> 
