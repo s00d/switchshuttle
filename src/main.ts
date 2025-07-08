3a3bc26af615a6a2aa4af04af7fe2574cde4c487
Pavel Kuzmin <Virus191288@gmail.com>
2025-07-08 15:08:25 +0500

    feat(router): replace update route with settings route

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./main.css"
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import TauriCommandsPlugin from './lib/tauri-commands-plugin';
 
 import About from './pages/About.vue';
 import Editor from './pages/Editor.vue';
@@ -9,7 +10,7 @@ import Help from './pages/Help.vue';
 import Inputs from './pages/Inputs.vue';
 import Loading from './pages/Loading.vue';
 import Main from './pages/Main.vue';
-import Update from './pages/Update.vue';
+import Settings from './pages/Settings.vue';
 
 const routes = [
     { path: '/', component: Main },
@@ -18,7 +19,7 @@ const routes = [
     { path: '/help', component: Help },
     { path: '/inputs/:id', component: Inputs },
     { path: '/loading', component: Loading },
-    { path: '/update', component: Update }
+    { path: '/settings', component: Settings }
 ];
 
 const router = createRouter({
@@ -38,6 +39,7 @@ router.onError((error) => {
 const app = createApp(App);
 
 app.use(router);
+app.use(TauriCommandsPlugin);
 
 app.mount('#app');
 
