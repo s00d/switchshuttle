c0bf32edd5fd1a4b2c4a596575ef56fde975cf71
Pavel Kuzmin <Virus191288@gmail.com>
2025-07-12 13:32:25 +0500

    refactor(lib): clean up import statements and format global shortcut handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,20 +12,20 @@ mod settings;
 
 use crate::cli::handle_cli_commands;
 use crate::commands::{
-    check_for_updates, create_new_config, create_new_configuration,
-    delete_configuration, duplicate_configuration, execute, execute_command_with_inputs,
-    fetch_input_data, get_config_files, get_configurations, get_settings,
-    get_settings_schema, get_terminals_list, get_unique_config_title, get_version, load_config, open_config_folder,
-    open_configuration, refresh_configurations, save_configuration_by_id,
-    save_or_update_configuration, save_settings, show_notification, validate_configuration,
+    check_for_updates, create_new_config, create_new_configuration, delete_configuration,
+    duplicate_configuration, execute, execute_command_with_inputs, fetch_input_data,
+    get_config_files, get_configurations, get_settings, get_settings_schema, get_terminals_list,
+    get_unique_config_title, get_version, load_config, open_config_folder, open_configuration,
+    refresh_configurations, save_configuration_by_id, save_or_update_configuration, save_settings,
+    show_notification, validate_configuration,
 };
-use crate::console::{ConsolePool};
-use crate::hotkeys::{HotkeyManager};
-use crate::menu::{create_system_tray_menu, handle_system_tray_event, execute_command_by_id};
+use crate::console::ConsolePool;
+use crate::hotkeys::HotkeyManager;
+use crate::menu::{create_system_tray_menu, execute_command_by_id, handle_system_tray_event};
 use crate::settings::AppSettings;
 use config::ConfigManager;
 use std::sync::{Arc, Mutex};
-use tauri::{Manager};
+use tauri::Manager;
 use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 use tauri_plugin_cli::CliExt;
 use tauri_plugin_global_shortcut::ShortcutState;
@@ -56,42 +56,56 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_global_shortcut::Builder::new()
-            .with_handler(move |app, shortcut, event| {
-                println!("[Hotkeys] Global shortcut event: {:?}, state: {:?}", shortcut, event.state());
-                
-                if event.state() == ShortcutState::Released {
-                    // Получаем зарегистрированные хоткеи
-                    let hotkey_manager = app.state::<Arc<Mutex<HotkeyManager>>>();
-                    let config_manager = app.state::<Arc<Mutex<ConfigManager>>>();
-                    let hotkey_manager = hotkey_manager.lock().unwrap();
-                    let config_manager = config_manager.lock().unwrap();
-                    
-                    // Ищем строку хоткея по shortcut
-                    let hotkey_str = hotkey_manager.registered_hotkeys.iter()
-                        .find(|(_, registered_shortcut)| *registered_shortcut == shortcut)
-                        .map(|(key, _)| key.as_str());
-                    
-                    if let Some(hotkey_str) = hotkey_str {
-                        // Ищем команду по хоткею
-                        if let Some((command, _config)) = hotkey_manager.find_command_by_hotkey(hotkey_str, &config_manager.configs) {
-                            if let Some(id) = &command.id {
-                                println!("[Hotkeys] Executing command: {} (id: {})", command.name, id);
-                                
-                                // Выполняем команду напрямую через единую функцию
-                                if let Err(e) = execute_command_by_id(&app, id, &config_manager) {
-                                    eprintln!("[Hotkeys] Failed to execute command: {}", e);
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(move |app, shortcut, event| {
+                    println!(
+                        "[Hotkeys] Global shortcut event: {:?}, state: {:?}",
+                        shortcut,
+                        event.state()
+                    );
+
+                    if event.state() == ShortcutState::Released {
+                        // Получаем зарегистрированные хоткеи
+                        let hotkey_manager = app.state::<Arc<Mutex<HotkeyManager>>>();
+                        let config_manager = app.state::<Arc<Mutex<ConfigManager>>>();
+                        let hotkey_manager = hotkey_manager.lock().unwrap();
+                        let config_manager = config_manager.lock().unwrap();
+
+                        // Ищем строку хоткея по shortcut
+                        let hotkey_str = hotkey_manager
+                            .registered_hotkeys
+                            .iter()
+                            .find(|(_, registered_shortcut)| *registered_shortcut == shortcut)
+                            .map(|(key, _)| key.as_str());
+
+                        if let Some(hotkey_str) = hotkey_str {
+                            // Ищем команду по хоткею
+                            if let Some((command, _config)) = hotkey_manager
+                                .find_command_by_hotkey(hotkey_str, &config_manager.configs)
+                            {
+                                if let Some(id) = &command.id {
+                                    println!(
+                                        "[Hotkeys] Executing command: {} (id: {})",
+                                        command.name, id
+                                    );
+
+                                    // Выполняем команду напрямую через единую функцию
+                                    if let Err(e) = execute_command_by_id(&app, id, &config_manager)
+                                    {
+                                        eprintln!("[Hotkeys] Failed to execute command: {}", e);
+                                    } else {
+                                        println!("[Hotkeys] Successfully executed command: {}", id);
+                                    }
                                 } else {
-                                    println!("[Hotkeys] Successfully executed command: {}", id);
+                                    eprintln!("[Hotkeys] Command has no ID: {}", command.name);
                                 }
-                            } else {
-                                eprintln!("[Hotkeys] Command has no ID: {}", command.name);
                             }
                         }
                     }
-                }
-            })
-            .build())
+                })
+                .build(),
+        )
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
@@ -185,7 +199,7 @@ pub fn run() {
             let hotkey_manager_clone = hotkey_manager.clone();
             let config_manager_clone = config_manager.clone();
             let settings_clone = settings.clone();
-            
+
             // ПЕРВИЧНАЯ РЕГИСТРАЦИЯ ГЛОБАЛЬНЫХ ХОТКЕЕВ ПРИ ЗАПУСКЕ
             println!("[Hotkeys] PRIMARY registration of all hotkeys at app startup");
             {
@@ -193,7 +207,9 @@ pub fn run() {
                 hotkey_manager.set_app_handle(app.handle().clone());
                 // Регистрируем все хоткеи из конфигураций
                 let configs = config_manager_clone.lock().unwrap().configs.clone();
-                if let Err(e) = hotkey_manager.register_all_hotkeys(&configs, &app.handle(), &settings_clone) {
+                if let Err(e) =
+                    hotkey_manager.register_all_hotkeys(&configs, &app.handle(), &settings_clone)
+                {
                     eprintln!("Failed to register hotkeys: {}", e);
                 }
             }
